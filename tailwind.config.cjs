6a10f65dbdc7a4f661bfcf241b4ebbcbe49e2b42
Trent Peterson <tdpeterson@gmail.com>
2023-12-13 12:33:55 -0800

    Add sign-in and docs links to footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,24 +5,24 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-  themes: [
-    {
-      planship: {
-        'primary': '#65b5f9',
-        'primary-content': '#fff4ee',
-        'secondary': '#53d4a4',
-        'accent': '#ff5149',
-        'neutral': '#190900',
-        'neutral-content': '#fff4ee',
-        'base-100': '#fff4ee',
-        'info': '#65b5f9',
-        'success': '#96cc37',
-        'success-content': '#fff4ee',
-        'warning': '#FBBD23',
-        'error': '#ff5149',
+    themes: [
+      {
+        planship: {
+          'primary': '#65b5f9',
+          'primary-content': '#fff4ee',
+          'secondary': '#53d4a4',
+          'accent': '#ff5149',
+          'neutral': '#190900',
+          'neutral-content': '#fff4ee',
+          'base-100': '#fff4ee',
+          'info': '#65b5f9',
+          'success': '#96cc37',
+          'success-content': '#fff4ee',
+          'warning': '#FBBD23',
+          'error': '#ff5149',
+        },
       },
-    },
-  ],
+    ],
   },
   plugins: [
     require('daisyui'),
