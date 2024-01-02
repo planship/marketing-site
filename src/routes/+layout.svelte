995ce8255592ad5db6231122d6bcefbe15e1a2e2
Trent Peterson <tdpeterson@gmail.com>
2024-01-02 12:10:40 -0800

    Update Umami script location

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
   <meta name="keywords" content="{keywords}"/>
 
   {#if !dev}
-    <script async src="https://cloud.umami.is/script.js" data-website-id="39e67e9e-ebe9-43b3-bce8-6a40d55c0b79"></script>
+    <script async src="https://us.umami.is/script.js" data-website-id="39e67e9e-ebe9-43b3-bce8-6a40d55c0b79"></script>
   {/if}
 </svelte:head>
 
